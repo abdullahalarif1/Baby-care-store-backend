745a8bf4375d1b10bf1ad24e73fc7f7a07416131
abdullahalarif1 <abdullahrabzil6@gmail.com>
2024-03-28 11:31:46 +0600

    all done

--- index.js
+++ index.js
@@ -75,72 +75,9 @@ async function run() {
 
 
 
-        // app.get("/api/winter-clothes/:id", async (req, res) => {
-        //     const id = req.params.id
-        //     const filter = { _id: new ObjectId(id) }
-        //     const result = await winterClothesCollection.findOne(filter)
-        //     res.send(result);
-        // });
+        
 
-        // // clothe get
-        // app.post("/api/winter-clothes", async (req, res) => {
-        //     const clothes = req.body;
-        //     const result = await winterClothesCollection.insertOne(clothes);
-        //     res.send(result);
-        // });
-
-        // // clothe delete
-        // app.delete("/api/winter-clothes/:id", async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await winterClothesCollection.deleteOne(query);
-        //     res.send(result);
-        // });
-
-        // //  update
-        // app.put("/api/winter-clothes/:id", async (req, res) => {
-        //     const id = req.params.id;
-        //     const clothes = req.body;
-        //     const filter = { _id: new ObjectId(id) };
-        //     const options = { upsert: true };
-        //     const updatedUser = {
-        //         $set: {
-        //             title: clothes.title,
-        //             category: clothes.category,
-        //             size: clothes.size,
-        //         },
-        //     };
-        //     const result = await winterClothesCollection.updateOne(
-        //         filter,
-        //         updatedUser,
-        //         options
-        //     );
-        //     res.send(result);
-        // });
-
-
-        // app.get("/api/donors", async (req, res) => {
-        //     const result = await donorsCollection.find().sort({ amount: -1 }).toArray();
-        //     res.send(result);
-        // });
-
-        // app.post("/api/donors", async (req, res) => {
-        //     const donors = req.body;
-        //     const result = await donorsCollection.insertOne(donors);
-        //     res.send(result);
-        // });
-
-        // // volunteer
-        // app.get("/api/volunteer", async (req, res) => {
-        //     const result = await volunteerCollection.find().toArray();
-        //     res.send(result);
-        // });
-
-        // app.post("/api/volunteer", async (req, res) => {
-        //     const volunteer = req.body;
-        //     const result = await volunteerCollection.insertOne(volunteer);
-        //     res.send(result);
-        // });
+       
 
         //==============================================================
 
